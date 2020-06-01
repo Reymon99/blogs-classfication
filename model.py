b049ec77f9be6a0d450c984a7f6555d8c74cf7e3
Sergio Majé <s.maje@udla.edu.co>
2020-06-01 01:24:37 -0500

    Implement common in model

--- model.py
+++ model.py
@@ -1,8 +1,9 @@
 import sys
 
 from google.api_core.client_options import ClientOptions
-from google.cloud import automl_v1
+from google.cloud.automl_v1 import PredictionServiceClient
 from google.cloud.automl_v1.proto import service_pb2
+from common import credentials
 
 def inline_text_payload(file_path):
   with open(file_path, 'rb') as ff:
@@ -14,18 +15,20 @@ def pdf_payload(file_path):
 
 def get_prediction(file_path, model_name):
   options = ClientOptions(api_endpoint='automl.googleapis.com')
-  prediction_client = automl_v1.PredictionServiceClient(client_options=options)
+  prediction_client = PredictionServiceClient(client_options=options)
 
   payload = inline_text_payload(file_path)
   # Uncomment the following line (and comment the above line) if want to predict on PDFs.
   # payload = pdf_payload(file_path)
+
+  print(payload)
 
   params = {}
   request = prediction_client.predict(model_name, payload, params)
   return request  # waits until request is returned
 
 if __name__ == '__main__':
-  file_path = sys.argv[1]
-  model_name = sys.argv[2]
+  file_path = "predict_data/" + sys.argv[1]
+  model_name = credentials()['model_name']
 
   print(get_prediction(file_path, model_name))
